02e114a80df18f6fc6da81eebafa6a9806341934
Dante Kelly <dante@dantekelly.com>
2024-05-31 18:59:02 -0600

    pass 3rd stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,17 +10,18 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		// Wait for user input
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "Error reading input: %s", err)
+		}
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintf(os.Stdout, "Error reading input: %s", err)
+		// Handle the command
+		handleCommand(command)
 	}
-
-	// Handle the command
-	handleCommand(command)
 }
 
 func handleCommand(command string) {
