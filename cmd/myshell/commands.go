9d2dbf716bdd9318ce689ef749a92c8611770585
Dante Kelly <dante@dantekelly.com>
2024-05-31 20:43:37 -0600

    pass 10th stage

---
+++ cmd/myshell/commands.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+// Handle Commands
+func handleCommand(s string) {
+	// Remove the newline character from the command
+	s = strings.TrimSuffix(s, "\n")
+	command := strings.Split(s, " ")[0]
+	args := strings.Split(s, " ")[1:]
+
+	switch command {
+	case "type":
+		typeCommand(args)
+	case "cd":
+		cdCommand(args)
+	case "pwd":
+		pwdCommand()
+	case "echo":
+		echoCommand(args)
+	case "exit":
+		os.Exit(0)
+	default:
+		runCommand(strings.Split(s, " "))
+	}
+}
+
+// Commands
+func cdCommand(args []string) {
+	command := args[0]
+
+	if err := os.Chdir(command); err != nil {
+		Print("%s: No such file or directory\n", command)
+	}
+}
+func echoCommand(args []string) {
+	str := strings.Join(args, " ")
+	Print("%s\n", str)
+}
+func pwdCommand() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Print("%s\n", dir)
+}
+func runCommand(args []string) {
+	command := args[0]
+
+	ok, p := searchEnvPath(command)
+	if ok {
+		executeProgram(p, args[1:])
+
+		return
+	}
+	if _, err := os.Stat(command); err == nil {
+		executeProgram(command, args[1:])
+
+		return
+	}
+
+	Print("%s: command not found\n", command)
+}
+func typeCommand(args []string) {
+	if len(args) >= 1 {
+		switch args[0] {
+		case "type", "echo", "exit":
+			Print("%s is a shell builtin\n", args[0])
+		default:
+			ok, p := searchEnvPath(args[0])
+			if ok {
+				Print("%s is %s\n", args[0], p)
+				return
+			}
+
+			Print("%s not found\n", args[0])
+		}
+
+		return
+	}
+
+	Print("type: missing argument\n")
+}
+
+// Utilities
+func executeProgram(p string, args []string) {
+	command := exec.Command(p, args...)
+	command.Stderr = os.Stderr
+	command.Stdout = os.Stdout
+
+	err := command.Run()
+	if err != nil {
+		log.Fatal("[FATAL] - ", err)
+	}
+}
+func searchEnvPath(command string) (bool, string) {
+	paths := strings.Split(os.Getenv("PATH"), ":")
+
+	for _, path := range paths {
+		fp := filepath.Join(path, command)
+		if _, err := os.Stat(fp); err == nil {
+			return true, fp
+		}
+	}
+
+	return false, ""
+}
--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"bufio"
-	"log"
 	"os"
-	"os/exec"
-	"path/filepath"
-	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	// fmt.Println("Logs from your program will appear here!")
+	handleInput()
+}
 
+func handleInput() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		Print("$ ")
+
 		// Wait for user input
 		command, err := reader.ReadString('\n')
 		if err != nil {
@@ -24,84 +22,5 @@ func main() {
 
 		// Handle the command
 		handleCommand(command)
-	}
-}
-
-func searchEnvPath(command string) (bool, string) {
-	paths := strings.Split(os.Getenv("PATH"), ":")
-
-	for _, path := range paths {
-		fp := filepath.Join(path, command)
-		if _, err := os.Stat(fp); err == nil {
-			return true, fp
-		}
-	}
-
-	return false, ""
-}
-
-func executeProgram(p string, args []string) {
-	command := exec.Command(p, args...)
-	command.Stderr = os.Stderr
-	command.Stdout = os.Stdout
-
-	err := command.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func handleCommand(s string) {
-	// Remove the newline character from the command
-	s = strings.TrimSuffix(s, "\n")
-	command := strings.Split(s, " ")[0]
-	args := strings.Split(s, " ")[1:]
-
-	switch command {
-	case "type":
-		if len(args) >= 1 {
-			switch args[0] {
-			case "type", "echo", "exit":
-				Print("%s is a shell builtin\n", args[0])
-			default:
-				ok, p := searchEnvPath(args[0])
-				if ok {
-					Print("%s is %s\n", args[0], p)
-					return
-				}
-
-				Print("%s not found\n", args[0])
-			}
-
-			return
-		}
-
-		Print("type: missing argument\n")
-	case "pwd":
-		dir, err := os.Getwd()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		Print("%s\n", dir)
-	case "echo":
-		str := strings.Join(args, " ")
-		Print("%s\n", str)
-	case "exit":
-		os.Exit(0)
-	default:
-		ok, p := searchEnvPath(command)
-		if ok {
-			executeProgram(p, args)
-
-			return
-		}
-		if _, err := os.Stat(command); err == nil {
-			executeProgram(command, args)
-
-			return
-		}
-
-		Print("%s: command not found\n", command)
 	}
 }
